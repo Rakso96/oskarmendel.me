5493ffd501f391025af9a158fe8d84275655eafd
Oskar Mendel <mendel.oskar@gmail.com>
2015-08-22 21:47:03 +0200

    Made the portfolio section responsive
    i had some problem fixing the responsive part of the portfolio section because
    the javascript function for resize was getting called in the wrong spot.
    I created a temp fix for the problem but its still there lurking.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,11 @@
 //The document ready function.
 $(function(){
 
+//Resize the portfolio list incase the window is small (Mobile adjustment)
+  if($(window).width() < 967){
+    addMarginList(100, ".previous-work");
+  }
+
 //When a website in the portfolio section is hovered
   $('.websites').hover(
     function(){
@@ -10,9 +15,48 @@ $(function(){
   );
 });
 
-/**
+/*  WINDOW REZIE EVENT HANDLER
+If the window is rezised do functions that depends on rezize so the site doesnt need manual refresh
+  *In our case here we add margin to our portfolio list incase the screen was rezized.
+'*/
+$(window).resize(function () {
+    if($(window).width() < 965){
+      if($(document).width() < 980){
+        addMarginList(100, ".previous-work");
+      }
+      else{
+        resetInfoMargin();
+      }
+    }
+    else{
+      resetInfoMargin();
+    }
+});
+
+/** REMOVE LIST ITEM CLASS
 Removes a class from sibling list items.
 */
 function removeListItemClass($ListItem){
   $($ListItem).siblings().removeClass("selected-website");
+}
+
+/* ADD MARGIN LIST
+Adds margin of your choise to target list.
+*/
+function addMarginList(marginAmmount, target){
+  var n = ($(window).width() / 2) - 140;
+  var x = 0;
+
+  $(target).children().each(function (){
+    $(this).children().css('left', (-100 * x) + 'px');
+    $(this).css('left', n + 'px');
+    n = n + marginAmmount;
+    x++;
+  });
+}
+
+function resetInfoMargin(){
+  $(".previous-work").children().each(function (){
+    $(this).children().removeAttr("style");
+  });
 }
