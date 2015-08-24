9c5960c9ac2f5e54a85c3fa23275d76691f4a2d2
Oskar Mendel <mendel.oskar@gmail.com>
2015-08-24 23:54:18 +0200

    Created a simple functional blog system
    Next up is to wire up the entries into a database.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express'); //Getting the framework express
 
 var path = require('path'); //Helps us with putting together paths
-var blogEngine = require('./lib/blogengine/blogengine.js');
+var blogEngine = require('./lib/blogengine/blogengine');
 
 /*
   ROUTES
 */
 var routes = require('./routes/index');
-var blog = require('./routes/blog');
+var blogRoute = require('./routes/blog'); //Alla routes som har med blog funktionen att göra.
 
 var app = express();
 
@@ -17,12 +17,15 @@ app.set('views', path.join(__dirname, 'views')); // Set the view directory to /v
 app.set('view engine', 'jade'); //Use jade as templating engine
 
 app.use('/', routes); //use the routes var if weget a request on the '/' (the root directory of the site)
-app.use('/blog', blog);
+app.use('/blog', blogRoute.list);
+app.use('/article/:id', blogRoute.single);
 
-app.get('/article/:id', function(req, res){
+//app.use('/blog', blog);
+
+/*app.get('/article/:id', function(req, res){
 	var entry = blogEngine.getBlogEntry(req.params.id);
 	res.render('article', {title: entry.title, blog:entry});
-});
+});*/
 
 /*app.get('/', function (req, res) { //Setting a route for the root directory on the site
   res.send('Hello World!'); //res = respond, how the server should respond to your request.
--- routes/blog.js
+++ routes/blog.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var blogEngine = require('../lib/blogengine/blogengine.js');
+var blogEngine = require('../lib/blogengine/blogengine');
 
-router.get('/blog', function(req, res){
-	
-});
+//get the whole list of blog entries.
+exports.list = function (req, res){
+	res.render('blog', {title: 'Hey', message: 'Hello there!', entries:blogEngine.getBlogEntries()});
+};
 
-/*router.get('/article/:id', function(req, res){
-	var entry = blogEngine.getBlogEntry(req.params.id);
-	res.render('article', {title:entry.title, blog:entry});
-});*/
-
-module.exports = router;
+//get single entry.
+exports.single = function(req, res){
+	var entry = blogEngine.getBlogEntry(req.params.id); //get the specific entry fron blogEngine
+    res.render('article',{title:entry.title, body:entry.body}); //Render the article and spit in the title and body content
+}
