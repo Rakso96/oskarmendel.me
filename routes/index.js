6ad73d973f31abfca47cef98dfe862db7a210fd3
Oskar Mendel <mendel.oskar@gmail.com>
2015-08-25 02:42:53 +0200

    Revert because of bug.

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res){ //Setting a route for the root directory on the site
-  res.render('index', {title: 'Hey', message: 'Hello there!'});//res = respond, how the server should respond to your request.
-});
-
-module.exports = router; //module exports is what we want to send back to the file who used this file.
